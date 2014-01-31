a2f36f5e11bfa1b0b2ccfaad0243888ce1470dd3
carolgit <ana.caroline.ferreir@gmail.com>
2014-01-31 20:42:00 -0200

    slf4j indexcontroller

--- SoftwareEstatisticoMaster/src/softwareestatistico/controller/IndexController.java
+++ SoftwareEstatisticoMaster/src/softwareestatistico/controller/IndexController.java
@@ -32,6 +32,8 @@ import model.Amostra;
 import model.FrequenciaAbsoluta1;
 import model.FrequenciaRelativa1;
 import model.ValorAmostra;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.xml.sax.SAXException;
 import util.ChartGenerate;
 import util.Open;
@@ -98,8 +100,11 @@ public class IndexController implements Initializable {
     private ObservableList dados;
     private List<ValorAmostra> vlrs;
     private List<FrequenciaRelativa1> frs;
+    //slf4j
+     Logger logger = LoggerFactory.getLogger(IndexController.class);
     
     public XYChart.Series<String,Double> createSeries(List<FrequenciaRelativa1> lfr){
+        logger.info("createSeries");
         XYChart.Series<String,Double> series=new XYChart.Series<String,Double>();
         for (int i = 0; i < lfr.size(); i++) {
             XYChart.Data<String,Double> hdata=new XYChart.Data<String, Double>(String.valueOf(lfr.get(i).getKey()),lfr.get(i).getValue());
@@ -108,6 +113,7 @@ public class IndexController implements Initializable {
         return series;
     }
     public XYChart.Series<String,Integer> createSeries2(List<FrequenciaAbsoluta1> lfa){
+        logger.info("createSeries2");
         XYChart.Series<String,Integer> series=new XYChart.Series<String,Integer>();
         for (int i = 0; i < lfa.size(); i++) {
             XYChart.Data<String,Integer> hdata=new XYChart.Data<String, Integer>(String.valueOf(lfa.get(i).getKey()),lfa.get(i).getValue());
@@ -118,37 +124,58 @@ public class IndexController implements Initializable {
     
     @FXML//arrumar
     private void handleAbrirAction(ActionEvent event) throws IOException, SAXException {
+        logger.info("handleAbrirAction");
+        try{
         Open open=new Open(OpenFile.getInstance().open(),dados);
         open.run();
+        }catch(Exception e){
+            logger.error(e.getMessage());
+        }
 //        dados.addAll();
         
     }
     
     @FXML
     private void handleAddAction(ActionEvent event) throws IOException, SAXException {
+       logger.info("handleAddAction");  
+       try{ 
        ValorAmostra vlr=new ValorAmostra();
        vlr.setValor(Double.parseDouble(textValor.getText()));
 //       vlrs.add(vlr);
        dados.add(vlr);
+       }catch(Exception e){
+           logger.error(e.getMessage());
+       }
     }
     @FXML
     private void handleDelAction(ActionEvent event) throws IOException, SAXException {
-       System.out.println("entra");
+       logger.info("handleDelAction"); 
+       try{
        ValorAmostra vlr;
        vlr=(ValorAmostra) lista_amostra.getSelectionModel().getSelectedItem();
 //       vlrs.add(vlr);
        dados.remove(vlr);
+       }catch(Exception e){
+           logger.error(e.getMessage());
+       }
     }
     @FXML
     private void handleSalvarAction(ActionEvent event) throws IOException, SAXException {
+        logger.info("handleSalvarAction");
+        try{
         Amostra amostra=new Amostra();
         for (ValorAmostra valorAmostra: vlrs) {
             amostra.setDados(valorAmostra);
         }
         SaveFile.getInstance().save(amostra);
+        }catch(Exception e){
+            logger.error(e.getMessage());
+        }
     }
     @FXML
     private void handleChartAction(ActionEvent event) throws IOException, SAXException {
+        logger.info("handleChartAction");
+        try{
         Amostra amostra=new Amostra();
         for (ValorAmostra valorAmostra: vlrs) {
             amostra.setDados(valorAmostra);
@@ -172,6 +199,9 @@ public class IndexController implements Initializable {
         XYChart.Series<String,Double> series=createSeries(amostra.getFr());
         XYChart.Series<String,Integer> series2=createSeries2(amostra.getFa());
         barChart.getData().add(series2);
+        }catch(Exception e){
+            logger.error(e.getMessage());
+        }
     }
     
     @Override
@@ -179,6 +209,8 @@ public class IndexController implements Initializable {
         vlrs=new ArrayList<>();
         dados= FXCollections.observableList(vlrs);
         lista_amostra.setItems(dados);
+       
+        logger.info("initialize");
     }    
 
 }
