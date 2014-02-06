a5619b150e80b8eb9d2307ef83925ef998ab999e
carolgit <ana.caroline.ferreir@gmail.com>
2014-02-06 15:15:08 -0200

    moda nova

--- SoftwareEstatisticoMaster/src/threads/Moda.java
+++ SoftwareEstatisticoMaster/src/threads/Moda.java
@@ -18,7 +18,7 @@ import model.ValorAmostra;
 public class Moda implements Runnable{
   
     private Amostra amostra;
-    private int maxfreq=0;
+    private int maxfreq=1;
     
     public Moda(Amostra amostra) {
         this.amostra = amostra;
@@ -26,14 +26,14 @@ public class Moda implements Runnable{
     @Override
     public void run() {
         double mo=Double.NaN;
-        List<Double> dd=new ArrayList<>();
-        for(ValorAmostra d: amostra.getDados()){
-            dd.add(d.getValor());
-        } 
-        for (Double d : dd) {
-            if(Collections.frequency(dd, d)>maxfreq){
+//        List<Double> dd=new ArrayList<>();
+//        for(ValorAmostra d: amostra.getDados()){
+//            dd.add(d.getValor());
+//        } 
+        for (Double d : amostra.valores_ordenados_amostra()) {
+            if(Collections.frequency(amostra.valores_ordenados_amostra(), d)>maxfreq){
                 mo=d;
-                maxfreq=Collections.frequency(dd, d);
+                maxfreq=Collections.frequency(amostra.valores_ordenados_amostra(), d);
             }
         }
         amostra.setModa(mo);
